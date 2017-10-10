96c571220e24c403df8d246addbafb848c6941d8
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-10 09:33:23 -0400

    setting up the app.js file

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 // require cloze flashcard module
 // require inquirer for getting user input at command line
 // require fs
+
+var RegFlashcard = require('./reg-flashcard.js');
+var ClozeFlashcard = require('./cloze-flashcard.js');
+var inquirer = require('inquirer');
+var fs = require('fs');
+
+
